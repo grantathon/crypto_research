b0b2377d9dd0ede1c1f9651d757fa60ecb95e478
grantathon <grant.bartel@gmail.com>
2021-09-22 08:56:56 +0300

    Extracted data retrieval functionality

--- coin_market_data.py
+++ coin_market_data.py
@@ -3,14 +3,14 @@ import data
 
 if __name__ == '__main__':
     # Load config file
-    with open('configs/dex_data_config.json') as f:
+    with open('configs/market_data_config.json') as f:
         config = json.load(f)
 
     # Prepare script parameters
     start_date = config['start_date']
     end_date = config['end_date']
-    dex_cg_ids = config['dex']
+    coin_cg_ids = config['coins']
 
     # Get data and save locally
-    dex_data = data.dex_trading_volume(dex_cg_ids, start_date, end_date)
-    dex_data.to_csv('output/dex_trading_volume.csv')
+    coin_data = data.coin_market_data(coin_cg_ids, start_date, end_date)
+    coin_data.to_csv('output/coin_market_data.csv')
